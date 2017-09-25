64cc23c63c38a31d743203e91e809111cb30e784
yoming410 <106021153@live.asia.edu.tw>
2017-09-25 15:05:31 +0800

    公升轉加崙
    106021153

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner  scn = new Scanner(System.in);
+        int a=scn.nextInt();
+        System.out.println(Math.round(a*0.26418*10)/10f);
+
+
+    }
+
+}

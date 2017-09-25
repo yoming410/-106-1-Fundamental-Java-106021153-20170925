4538a385d10a26b786dd4b8ffe3cdba72bfc0679
yoming410 <106021153@live.asia.edu.tw>
2017-09-25 16:00:52 +0800

    Main2

--- Main2.java
+++ Main2.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
         Scanner  scn = new Scanner(System.in);
         int a=scn.nextInt();
-        System.out.println(Math.round(a*0.26418*10)/10f);
+        System.out.println(Math.round(a/600f*10)/10f);
     }
 }
